c51077363b26d60926e55e014a781b8398fa960e
shippydip <shippydip@cp.com>
2025-12-30 15:18:20 -0800

    Implement part 2.1

--- backend/src/controllers/tasks.ts
+++ backend/src/controllers/tasks.ts
@@ -4,7 +4,7 @@ import type { RequestHandler } from "express";
 
 export const getAllTasks: RequestHandler = async (req, res, next) => {
   try {
-    const tasks = await TaskModel.find().sort({ dateCreated: "desc" });
+    const tasks = await TaskModel.find().sort({ dateCreated: "desc" }).populate("assignee");
     res.status(200).json(tasks);
   } catch (error) {
     next(error);
